4fd26ecc3ba3b8f2291e427fa795591af5430612
sdana <seth.dana@gmail.com>
2018-06-01 15:01:58 -0500

    still not wrapping my head around how to do the last 2 challenegs. attempted some changes but nothing worked

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
-const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60]
+const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60, 45, 67, 68]
 let grades = {} // You'll need to change this line of code
 let currentGradeCount = []
+let arrayIndexValue = 0
+const getGradeCount = () => {
+
+}
+getGradeCount()
 
 for (let grade in scores) {
 
     if (scores[grade] <=100 && scores[grade] >=91){
         grades["A"] = (grades["A"]+1) || 1
-        currentGradeCount[0] = (currentGradeCount[0] + 1) || 1
+        // currentGradeCount[0] = (currentGradeCount[0] + 1) || 1
     }
     else if (scores[grade] <= 90 && scores[grade] >= 81) {
         grades["B"] = (grades["B"]+1) || 1
-        currentGradeCount[1] = (currentGradeCount[1] + 1) || 1
+        // currentGradeCount[1] = (currentGradeCount[1] + 1) || 1
     }
     else if (scores[grade] <= 80 && scores[grade] >= 71) {
         grades["C"] = (grades["C"]+1) || 1
-        currentGradeCount[2] = (currentGradeCount[2] + 1) || 1
+        // currentGradeCount[2] = (currentGradeCount[2] + 1) || 1
     }
     else if (scores[grade] <= 70 && scores[grade] >= 61) {
         grades["D"] = (grades["D"]+1) || 1
-        currentGradeCount[3] = (currentGradeCount[3] + 1) || 1
+        // currentGradeCount[3] = (currentGradeCount[3] + 1) || 1
     }
     else if (scores[grade] <= 60){
         grades["F"] = (grades["F"]+1) || 1
-        currentGradeCount[4] = (currentGradeCount[0] + 1) || 1
+        // currentGradeCount[4] = (currentGradeCount[0] + 1) || 1
     }
-    console.log(currentGradeCount)
+}
+for (let gradeType in grades){
+    console.log(gradeType)
 }
 scores.sort(function(a,b){return(a-b)})
 console.log(`The lowest score is ${scores[0]}`)
